f8637aa1ee5a439b734f9603db4f7e65a205cc0c
George Livas <giorgos.livas.99@gmail.com>
2019-11-28 19:32:37 +0000

    add face tracking

---
+++ outline.py
@@ -0,0 +1,23 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# read the image
+image = cv2.imread("images/g2.JPG")
+# convert to RGB
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# create a binary thresholded image
+_, binary = cv2.threshold(gray, 100, 100, cv2.THRESH_BINARY_INV)
+# show it
+plt.imshow(binary, cmap="gray")
+plt.show()
+# find the contours from the thresholded image
+contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# draw all contours
+image = cv2.drawContours(image, contours, 1, (0, 255, 0), 2)
+# show the image with the drawn contours
+plt.imshow(gray)
+
+plt.imshow(image)
+plt.show()
