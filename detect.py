5105c1394add839000e729e7a12b16ee30ca6a6e
George Livas <giorgos.livas.99@gmail.com>
2020-02-25 16:14:29 +0000

    add ids

--- detect.py
+++ detect.py
@@ -2,13 +2,17 @@ from imutils.video import VideoStream
 from get_similarity import get_similarity
 import numpy as np
 import cv2
+import dlib
+
 import time
 import os
 
 from pyimagesearch.centroidtracker import CentroidTracker
 from pyimagesearch.trackableobject import TrackableObject
 
-net = cv2.dnn.readNetFromCaffe('/Users/georgelivas/PycharmProjects/humanDetection/mobilenet_ssd/MobileNetSSD_deploy.prototxt', '/Users/georgelivas/PycharmProjects/humanDetection/mobilenet_ssd/MobileNetSSD_deploy.caffemodel')
+net = cv2.dnn.readNetFromCaffe(
+	'/Users/georgelivas/PycharmProjects/humanDetection/mobilenet_ssd/MobileNetSSD_deploy.prototxt',
+	'/Users/georgelivas/PycharmProjects/humanDetection/mobilenet_ssd/MobileNetSSD_deploy.caffemodel')
 
 cv2.startWindowThread()
 
@@ -29,6 +33,9 @@ ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
 W = None
 H = None
 
+trackers = []
+trackableObjects = {}
+
 while True:
 	frame = cap.read()
 	frame = cv2.resize(frame, (640, 480))
@@ -44,6 +51,7 @@ while True:
 	detections = net.forward()
 
 	boxes = []
+	rects = []
 
 	# loop over the detections
 	for i in np.arange(0, detections.shape[2]):
@@ -51,7 +59,9 @@ while True:
 		# with the prediction
 		confidence = detections[0, 0, i, 2]
 
-		if confidence > 0.5:
+		trackers = []
+
+		if confidence > 0.6:
 			# extract the index of the class label from the
 			# detections list
 			idx = int(detections[0, 0, i, 1])
@@ -78,11 +88,51 @@ while True:
 			boxes.append(box.astype('int'))
 
 			(startX, startY, endX, endY) = box.astype('int')
-			# cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-			cv2.circle(frame, (int((startX + endX)/2), startY-10), 4, (0, 255, 0), -1)
+
+			tracker = dlib.correlation_tracker()
+			rect = dlib.rectangle(startX, startY, endX, endY)
+			tracker.start_track(rgb, rect)
+
+			trackers.append(tracker)
+
+	for tracker in trackers:
+		tracker.update(rgb)
+		pos = tracker.get_position()
+
+		# unpack the position object
+		startX = int(pos.left())
+		startY = int(pos.top())
+		endX = int(pos.right())
+		endY = int(pos.bottom())
+
+		# add the bounding box coordinates to the rectangles list
+		rects.append((startX, startY, endX, endY))
+
+	objects = ct.update(rects)
+
+	for (objectID, centroid) in objects.items():
+		# check to see if a trackable object exists for the current
+		# object ID
+		# to = trackableObjects.get(objectID, None)
+
+		# if there is no existing trackable object, create one
+		# if to is None:
+		# to = TrackableObject(objectID, centroid)
+		#
+		# # store the trackable object in our dictionary
+		# trackableObjects[objectID] = to
+
+		# (startX, startY, endX, endY) = box.astype('int')
+		# # cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+		# cv2.circle(frame, (int((startX + endX) / 2), startY - 10), 8, (0, 255, 0), -1)
+		#
+		text = "ID {}".format(objectID)
+		cv2.putText(frame, text, (centroid[0] - 10, int(centroid[1]/2) - 10),
+		            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		cv2.circle(frame, (centroid[0], int(centroid[1]/2)), 4, (0, 255, 0), -1)
 
 	cv2.imshow('Pheebs', frame)
-	if cv2.waitKey(100) & 0xFF == ord('q'):
+	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
 cap.release()
